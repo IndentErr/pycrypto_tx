86d8574d0740123410f2564ba0285cd9d809092c
IndentErr <62327889+IndentErr@users.noreply.github.com>
2022-04-30 15:42:00 -0700

    git commit

--- setup.py
+++ setup.py
@@ -11,7 +11,9 @@ license = "MIT",
 author = "IndentErr",
 description = "Free and easy python cryptocurrency transaction library",
 url = "https://github.com/IndentErr/pycrypto_tx",
-packages = find_packages("src"),
+packages = find_packages(),
 install_requires = ["cryptocompare","datetime","logging","web3"],
-python_version = ">=3.6"
+python_version = ">=3.6",
+long_description=open('README.md').read(),
+
 )
