ca29de7f1088322c7c1941b6267200f82ca4b08d
IndentErr <62327889+IndentErr@users.noreply.github.com>
2022-05-14 15:44:53 -0700

    git commit

--- codes/setup.py
+++ codes/setup.py
@@ -6,7 +6,7 @@ with open("README.md") as fh:
 
 setup(
 name = "pycrypto_tx - IndentErr",
-version = "0.1",
+version = "0.1Alpha",
 license = "MIT",
 author = "IndentErr",
 description = "Free and easy python cryptocurrency transaction library",
@@ -15,4 +15,7 @@ packages = find_packages(),
 install_requires = ["cryptocompare","datetime","logging","web3","secrets","codecs","Crypto.Hash","ecdsa","re","bitcoinlib.wallets","base58","hashlib"],
 python_version = ">=3.6",
 long_description=open('README.md').read(),
+classifiers = ["Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",]
 )
