061dfabe6cc1427e6c5d411df58b653273fc9af8
IndentErr <62327889+IndentErr@users.noreply.github.com>
2022-05-14 15:58:39 -0700

    Revert "git commit"
    This reverts commit caa1ac342d82046c4a7ab566ae36bf5393b0742c.

--- codes/setup.py
+++ codes/setup.py
@@ -6,7 +6,7 @@ with open("README.md") as fh:
 
 setup(
 name = "pycrypto_tx - IndentErr",
-version = "0.1Alpha",
+version = "0.1",
 license = "MIT",
 author = "IndentErr",
 description = "Free and easy python cryptocurrency transaction library",
@@ -15,7 +15,4 @@ packages = find_packages(),
 install_requires = ["cryptocompare","datetime","logging","web3","secrets","codecs","Crypto.Hash","ecdsa","re","bitcoinlib.wallets","base58","hashlib"],
 python_version = ">=3.6",
 long_description=open('README.md').read(),
-classifiers = ["Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",]
 )
