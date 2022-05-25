15d0c3f4279084fcd9c2413897c126932bdca220
IndentErr <62327889+IndentErr@users.noreply.github.com>
2022-05-25 11:29:03 -0700

    git commit

--- codes/setup.py
+++ codes/setup.py
@@ -1,9 +1,5 @@
 from setuptools import setup, find_packages
 
-with open("README.md") as fh:
-    long_description = fh.read()
-
-
 setup(
 name = "pycrypto_tx - IndentErr",
 version = "0.1",
@@ -14,5 +10,4 @@ url = "https://github.com/IndentErr/pycrypto_tx",
 packages = find_packages(),
 install_requires = ["cryptocompare","datetime","web3","cryptodome","ecdsa","bitcoinlib","base58","hashlib"],
 python_version = ">=3.6",
-long_description=open('README.md').read(),
 )
